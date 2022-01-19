aaa926beb4f52fd4bf1bb54e1343bc806dcf3783
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-01-19 15:48:41 -0700

    feat(scrapper): complete Washingtons scrapper, standardize types

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,32 +1,50 @@
-import cheerio from "cheerio";
+import cheerio, { CheerioAPI } from "cheerio";
 import axios from "axios";
 
-abstract class Scraper {
-  abstract get(query: string): Promise<string[]>;
-}
+type ScrapperFactory = (
+  concertCalendarUrl: string,
+  artistNameBuilder: ($: CheerioAPI) => string[],
+) => Promise<string[]>;
 
-type Scrapper = (url: string, querySelector: string) => string[];
-
-(async () => {
-  const res = await axios.get("https://washingtonsfoco.com/events/");
-
+const scrapperFactory: ScrapperFactory = async (url, builder) => {
+  const res = await axios.get(url);
   const $ = cheerio.load(res.data);
+  return builder($);
+};
 
-  const extractHeading = (node: Record<string, any>): string =>
-    "data" in node.children[0] ? (node.children[0] as any).data : "";
+export type Scrapper = () => Promise<string[]>;
 
-  const remove = (regex: string | RegExp) =>
-    (title: string) => title.replace(regex, "").trim();
-  const removeWhitespace = remove(/\n\t/g);
-  const removeBoldNotation = remove(/\*{1,2}.+\*{1,2}\s+/g);
-  const removeAnEveningWith = remove(/An Evening with /g);
-  const removeSuffix = remove(/ –.+/g);
+export const washingtonsScrapper: Scrapper = () =>
+  scrapperFactory(
+    "https://washingtonsfoco.com/events/",
+    ($) => {
+      const extractHeading = (node: Record<string, any>): string =>
+        "data" in node.children[0] ? (node.children[0] as any).data : "";
 
-  const elements = $("h2.font1by25.font1By5remMD").toArray().map(extractHeading)
-    .map(removeWhitespace)
-    .map(removeBoldNotation)
-    .map(removeAnEveningWith)
-    .map(removeSuffix);
+      const remove = (regex: string | RegExp) =>
+        (title: string) => title.replace(regex, "").trim();
+      const removeWhitespace = remove(/\n\t/g);
+      const removeBoldNotation = remove(/\*{1,2}.+\*{1,2}\s+/g);
+      const removeAnEveningWith = remove(/An Evening with /g);
+      const removeSuffix = remove(/ –.+/g);
+      const removeAfterAmpersand = remove(/ &.+/g);
+      const removeAfterAnd = remove(/ and .+/g);
+      const removeAfterBand = remove(/ Band$/g);
 
-  console.log(elements, typeof elements);
-})();
+      const elementsToArtistNames = $("h2.font1by25.font1By5remMD").toArray()
+        .map(extractHeading)
+        .map(removeWhitespace)
+        .map(removeBoldNotation)
+        .map(removeAnEveningWith)
+        .map(removeSuffix)
+        .map(removeAfterAmpersand)
+        .map(removeAfterAnd)
+        .map(removeAfterBand);
+
+      const unique = (value: string, index: number, self: string[]) =>
+        self.indexOf(value) === index;
+      const uniqueArtistNames = elementsToArtistNames.filter(unique);
+
+      return uniqueArtistNames;
+    },
+  );
