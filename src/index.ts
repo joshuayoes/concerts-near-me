517a89759841d9813e17f83978f205f247a12f02
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-01-19 16:42:39 -0700

    feat(index): created playlist for Washington's concert venue

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,76 @@
 import api from "./api";
+import { log, logWarn } from "./logger";
+import { washingtonsScrapper } from "./scrape";
+
+const getArtistBySearch = async (query: string) => {
+  const res = await api.search(query, ["artist"]);
+  const [artist] = res.body.artists?.items ?? [];
+  if (!artist) {
+    throw Error(`${query} was not found on Spotify`);
+  }
+  return artist;
+};
+
+export const createPlaylist = async (name: string) => {
+  const id = null;
+
+  if (!id) {
+    const res = await api.createPlaylist(name, {
+      public: true,
+    });
+
+    log(`Playlist "${name}" created: ${res.body.uri}`);
+
+    return res.body.id;
+  }
+
+  return id;
+};
+
+const getTopTracksIds = async (artistId: string, maxSize = 5) => {
+  const tracks = await api.getArtistTopTracks(artistId, "US");
+  return tracks.body.tracks.map(({ id }) => id).filter((_, index) =>
+    index < maxSize
+  );
+};
 
 (async () => {
-  const getArtistBySearch = async (query: string) => {
-    const res = await api.search(query, ["artist"]);
-    const [artist] = res.body.artists?.items ?? [];
-    if (!artist) throw Error("No artist");
-    return artist;
-  };
+  const artistsNames = await washingtonsScrapper();
 
-  const createPlaylist = async (name: string) => {
-    const id = null;
+  const playlistName = "Washington's Upcoming Artists";
+  const playlistId = await createPlaylist(playlistName);
+  const allTopTrackUris: string[] = [];
 
-    if (!id) {
-      const res = await api.createPlaylist(name, {
-        public: true,
-      });
-
-      return res.body.id;
+  for (const name of artistsNames) {
+    try {
+      const artist = await getArtistBySearch(name);
+      const topTracksIds = await getTopTracksIds(artist.id);
+      const topTracksUris = topTracksIds.map((id) => `spotify:track:${id}`);
+      allTopTrackUris.push(...topTracksUris);
+      log(`${name} added to playlist`);
+    } catch (error) {
+      if (error instanceof Error) {
+        logWarn(error.message);
+      }
     }
+  }
 
-    return id;
-  };
+  // split into chunks of 100
+  const chunks = allTopTrackUris.reduce(
+    (acc, curr, index) => {
+      const chunkIndex = Math.floor(index / 100);
+      if (!acc[chunkIndex]) {
+        acc[chunkIndex] = [];
+      }
+      acc[chunkIndex].push(curr);
+      return acc;
+    },
+    [] as string[][],
+  );
 
-  const getTopTracksIds = async (artistId: string, maxSize = 5) => {
-    const tracks = await api.getArtistTopTracks(artistId, "US");
-    return tracks.body.tracks.map(({ id }) => id).filter((_, index) =>
-      index < maxSize
-    );
-  };
+  for (const chunk of chunks) {
+    await api.addTracksToPlaylist(playlistId, chunk);
+  }
 
-  const artist = await getArtistBySearch("Goth Babe");
-  const topTracksIds = await getTopTracksIds(artist.id);
-  const topTracksUri = topTracksIds.map((id) => `spotify:track:${id}`);
-  const playlistId = await createPlaylist("Concert Venue");
-
-  const res = await api.addTracksToPlaylist(playlistId, topTracksUri);
-
-  console.log(res);
+  log(`Playlist "${playlistName}" updated`);
 })();
