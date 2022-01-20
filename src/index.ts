6618f2eef6c6678ec5a71e93661c374f354c123c
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-01-19 18:01:32 -0700

    chore(index): update order of events

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,13 @@ import {
 } from "./spotify";
 
 (async () => {
-  const playlistName = "Washington's Upcoming Artists";
-  const playlistDescription =
-    "The most popular tracks from artists performing at Washington's soon: https://washingtonsfoco.com/events/";
-  const playlistId = await createPlaylist(playlistName, playlistDescription);
   const artistsNames = await washingtonsScrapper();
   const allTopTrackUris = await getManyArtistsTopTracksBySearch(artistsNames);
 
+  const playlistName = "Washington's Upcoming Artists";
+  const playlistDescription =
+    "The most popular tracks from artists performing at Washington's soon: https://washingtonsfoco.com/events/";
+
+  const playlistId = await createPlaylist(playlistName, playlistDescription);
   await addTracksToPlaylist(playlistId, allTopTrackUris);
 })();
