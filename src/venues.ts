e741b4ea7217bf72adf57a8f9526afd70bd5dfc2
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-02-04 22:11:20 -0700

    feat(venues): add module for handling venues

---
+++ src/venues.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+import fs from "fs/promises";
+import paths from "path";
+
+const VenueSchema = z.object({
+  playlistId: z.string(),
+  url: z.string(),
+});
+
+const VenuesSchema = z.array(VenueSchema);
+
+export const getVenues = async (jsonPath = "/venues.json") => {
+  const pwd = process.cwd();
+  const filePath = paths.join(pwd, jsonPath);
+
+  const buffer = await fs.readFile(filePath);
+  const json = JSON.parse(buffer.toString());
+
+  return VenuesSchema.parse(json);
+};
