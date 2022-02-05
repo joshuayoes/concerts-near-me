01d4a023d868d45700a392461ef5233b0a63dccb
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-02-04 17:59:57 -0700

    chore(config): validate conifg elements

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,20 @@
 import "dotenv/config";
 
 export default class Config {
-  static readonly clientId: string = process.env.CLIENT_ID!;
-  static readonly clientSecret: string = process.env.CLIENT_SECRET!;
-  static readonly redirectUri: string = process.env.REDIRECT_URI!;
-  static readonly accessToken: string = process.env.ACCESS_TOKEN!;
+  static readonly clientId: string = this.isString(process.env.CLIENT_ID);
+  static readonly clientSecret: string = this.isString(
+    process.env.CLIENT_SECRET,
+  );
+  static readonly redirectUri: string = this.isString(process.env.REDIRECT_URI);
+  static readonly accessToken: string = this.isString(process.env.ACCESS_TOKEN);
+
+  static isString(value: unknown): string {
+    if (!value) {
+      throw new Error("Value is undefined");
+    }
+    if (typeof value !== "string") {
+      throw new Error("Value is not a string");
+    }
+    return value;
+  }
 }
