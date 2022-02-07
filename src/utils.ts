aec31e16e82d06ae8bd0775f266094cba9e84c29
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-02-06 22:04:23 -0700

    chore(utils): add ROOT_DIR constant

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,8 @@ import path from "path";
 
 export const MOCK_DIR = path.join(__dirname, "__tests__", "__mocks__");
 
+export const ROOT_DIR = path.join(__dirname, "../");
+
 export const getLocalHtml = async (fileName: string) => {
   const html = await fs.readFile(`${MOCK_DIR}/${fileName}`, "utf8");
   return html;
