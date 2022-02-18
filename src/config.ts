dec9a5c28f76bac6aacdbb2b77372e1aecbdd5e4
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-02-18 16:49:30 -0700

    refactor(config): extract method into function

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,24 @@
 import "dotenv/config";
 
-export default class Config {
-  static readonly clientId: string = this.env("CLIENT_ID");
-  static readonly clientSecret: string = this.env("CLIENT_SECRET");
-  static readonly redirectUri: string = this.env("REDIRECT_URI");
-  static readonly accessToken: string = this.env("ACCESS_TOKEN");
+const env = (key: string): string => {
+  const value: unknown = process.env[key];
 
-  static env(key: string): string {
-    const value: unknown = process.env[key];
-
-    if (!value) {
-      throw new Error(`Value for ${key} in .env is "${typeof value}"`);
-    }
-
-    if (typeof value !== "string") {
-      throw new Error(
-        `Value for ${key} in .env is not a string, but "${typeof value}"`,
-      );
-    }
-
-    return value;
+  if (!value) {
+    throw new Error(`Value for ${key} in .env is "${typeof value}"`);
   }
+
+  if (typeof value !== "string") {
+    throw new Error(
+      `Value for ${key} in .env is not a string, but "${typeof value}"`,
+    );
+  }
+
+  return value;
+};
+
+export default class Config {
+  static readonly clientId: string = env("CLIENT_ID");
+  static readonly clientSecret: string = env("CLIENT_SECRET");
+  static readonly redirectUri: string = env("REDIRECT_URI");
+  static readonly accessToken: string = env("ACCESS_TOKEN");
 }
