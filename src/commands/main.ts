cfa6f11f65fe8d742cd5c7fbb5398ded0d32ec27
Joshua Yoes <37849890+joshuayoes@users.noreply.github.com>
2022-02-20 19:03:11 -0700

    feat(commands): add filter and dry args to main command
    * feat(commands): add filter option to main script
    
    * chore(main): add dry run option
    
    Co-authored-by: Joshua Yoes <joshua.r.yoes@gmail.com>

---
+++ src/commands/main.ts
@@ -0,0 +1,53 @@
+import logger from "../logger";
+import { findVenueByUrl } from "../scrape";
+import {
+  addTracksToPlaylist,
+  emptyPlaylist,
+  getManyArtistsTopTracksBySearch,
+} from "../spotify";
+import { extractPlaylistId, getPlaylists, Playlist } from "../playlists";
+
+interface Options {
+  filter?: string;
+  dry?: boolean;
+}
+
+const updateAllPlaylists = async ({ filter, dry }: Options = {}) => {
+  const playlists = await getPlaylists();
+
+  const updatePlaylist = async (playlist: Playlist) => {
+    const { venueUrl, playlistUrl } = playlist;
+    const playlistId = extractPlaylistId(playlistUrl);
+
+    const venue = findVenueByUrl(venueUrl);
+
+    if (!venue) {
+      logger.warn(`No scrapper found for "${venueUrl}"`);
+      return;
+    }
+
+    logger.info(`Scrapping ${venue.name} at "${venueUrl}"...`);
+    const artistsNames = await venue.scrape();
+    const allTopTrackUris = await getManyArtistsTopTracksBySearch(artistsNames);
+
+    if (dry) {
+      logger.info(`Dry run: not adding tracks to playlist`);
+      return;
+    }
+
+    await emptyPlaylist(playlistId);
+    await addTracksToPlaylist(playlistId, allTopTrackUris);
+  };
+
+  for (const playlist of playlists) {
+    const filterRegex = filter ? new RegExp(filter) : undefined;
+
+    if (filterRegex && !filterRegex.test(playlist.venueUrl)) {
+      continue;
+    }
+
+    await updatePlaylist(playlist);
+  }
+};
+
+export default updateAllPlaylists;
