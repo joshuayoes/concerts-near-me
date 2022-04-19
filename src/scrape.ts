56fd4e4787e9bbee2d9d8f598003f81d74dc80f7
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-04-19 10:35:35 -0600

    Revert "chore(scrape): add request headers"
    This reverts commit 1a8857bc8f94f5cbea8464b7931d75031962704d.

--- src/scrape.ts
+++ src/scrape.ts
@@ -1,27 +1,8 @@
 import cheerio, { CheerioAPI } from "cheerio";
-import axios, { AxiosRequestHeaders } from "axios";
+import axios from "axios";
 import axiosRetry from "axios-retry";
 
 // #region Core Library
-/** Headers to make request seem more like browser traffic */
-const headers: AxiosRequestHeaders = {
-  "User-Agent":
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
-  "Accept":
-    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-  "Accept-Language": "en-US,en;q=0.5",
-  "Accept-Encoding": "gzip, deflate, br",
-  "DNT": "1",
-  "Connection": "keep-alive",
-  "Upgrade-Insecure-Requests": "1",
-  "Sec-Fetch-Dest": "document",
-  "Sec-Fetch-Mode": "navigate",
-  "Sec-Fetch-Site": "cross-site",
-  "Pragma": "no-cache",
-  "Cache-Control": "no-cache",
-  "TE": "trailers",
-};
-
 /** Perform a GET request to URL and reutrn HTML payload */
 export const getHtml = async (url: string): Promise<string> => {
   axiosRetry(axios, {
@@ -32,7 +13,6 @@ export const getHtml = async (url: string): Promise<string> => {
   return axios.get<string>(url, {
     timeout: 15 * 1000,
     transitional: { clarifyTimeoutError: true },
-    headers,
   }).then(({ data }) => data);
 };
 
