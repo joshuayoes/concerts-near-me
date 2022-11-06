34f123902ed1f642a83bf069a74f90ad28ca5bc0
Joshua Yoes <joshua.r.yoes@gmail.com>
2022-11-05 18:36:29 -0700

    refactor: remove getAccessTokenViaBrowserLogin

--- src/commands/auth.ts
+++ src/commands/auth.ts
@@ -1,64 +1,14 @@
 import fs from "fs/promises";
-import puppeteer from "puppeteer";
 import { stringify } from "envfile";
 
 import Config, { EnvironmentVariables } from "../config";
-import { argvFactory, ROOT_DIR, toAsterisks } from "../utils";
+import { ROOT_DIR } from "../utils";
 import logger from "../logger";
 import axios from "axios";
 
 type AccessTokenFactory<Config> = (
   config: Config,
 ) => Promise<string | undefined>;
-
-interface BrowserLoginConfig {
-  username: string;
-  password: string;
-  url: string;
-}
-/** @deprecated - works on local machines, but not in Github Actions enviroment */
-export const getAccessTokenViaBrowserLogin: AccessTokenFactory<
-  BrowserLoginConfig
-> = async ({ username, password, url }) => {
-  let browser: puppeteer.Browser | undefined = undefined;
-  let token: string | undefined = undefined;
-
-  try {
-    // Get accessToken from Spotify using proxy server
-    const argv = argvFactory({ headless: { type: "boolean", default: true } });
-
-    browser = await puppeteer.launch({
-      ignoreDefaultArgs: ["--disable-extensions", "--no-sandbox"],
-      headless: argv.headless,
-    });
-    logger.info("Launched browser");
-    const page = await browser.newPage();
-    await page.goto(url);
-    logger.info(`Navigated to "${url}"`);
-    await page.type("input#login-username", username);
-    logger.info(`Inputed username: "${username}"`);
-    await page.type("input#login-password", password);
-    logger.info(`Inputed password: "${toAsterisks(password)}"`);
-    await page.click("button#login-button");
-    logger.info(`Clicked submit button`);
-    await page.waitForNavigation();
-
-    const response = await page.content();
-    logger.info(`Recieved page response`);
-    const extractToken = (str: string) => str.match(/"token":"([\w|-]+)"/)![1];
-    token = extractToken(response);
-    logger.info(`Extracted token from response`);
-  } catch (error) {
-    if (error instanceof Error) {
-      logger.error(error.message);
-    }
-  } finally {
-    await browser?.close();
-    logger.info(`Closed browser`);
-  }
-
-  return token;
-};
 
 interface RefreshTokenConfig {
   REFRESH_TOKEN: string;
